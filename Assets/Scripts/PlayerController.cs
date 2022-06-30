ca10bb18e6837b14d036b1f1205ab635d5a94eca
Bryce Sandall <86949903+bsandall2@users.noreply.github.com>
2022-06-30 19:33:14 +1000

    Distance UI

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,7 +6,11 @@ public class PlayerController : MonoBehaviour
 {
     public float gravity;
     public Vector2 velocity;
+    public float maxAcceleration = 10;
+    public float acceleration = 10;
+    public float distance = 0;
     public float jumpVelocity = 20;
+    public float maxXVelocity = 100;
     public float groundHeight = 10;
     public bool isGrounded = false;
 
@@ -70,6 +74,21 @@ public class PlayerController : MonoBehaviour
             {
                 pos.y = groundHeight;
                 isGrounded = true;               
+            }
+        }
+
+        distance += velocity.x * Time.fixedDeltaTime;
+
+        if (isGrounded)
+        {
+            float velocityRatio = velocity.x / maxXVelocity;
+            acceleration = maxAcceleration * (1 - velocityRatio);
+
+            velocity.x += acceleration * Time.fixedDeltaTime;
+            
+            if (velocity.x >= maxXVelocity)
+            {
+                velocity.x = maxXVelocity;
             }
         }
 
