84f759e79bfb69a2c2db4eddcfa8a1a38cabc620
Bryce Sandall <1024240@student.sae.edu.au>
2022-07-09 09:33:55 +1000

    RetryScreenPopup

--- Assets/Scripts/UIcontroller.cs
+++ Assets/Scripts/UIcontroller.cs
@@ -2,16 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class UIcontroller : MonoBehaviour
 {
     PlayerController player;
     Text distanceText;
 
+    GameObject results;
+    Text finalDistanceText;
+
     public void Awake()
     {
         player = GameObject.Find("Player").GetComponent<PlayerController>();
         distanceText = GameObject.Find("DistanceText").GetComponent<Text>();
+
+        finalDistanceText = GameObject.Find("FinalDistanceText").GetComponent<Text>();
+        results = GameObject.Find("Results");
+        results.SetActive(false);       
     }
     // Start is called before the first frame update
     void Start()
@@ -23,6 +31,22 @@ public class UIcontroller : MonoBehaviour
     void Update()
     {
         int distance = Mathf.FloorToInt(player.distance);
-        distanceText.text = distance + " m";        
+        distanceText.text = distance + " m";
+        
+        if (player.isDead)
+        {
+            results.SetActive(true);
+            finalDistanceText.text = distance + " m";
+        }
+    }
+
+    public void Quit()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void Retry()
+    {
+        SceneManager.LoadScene("LevelScene");
     }
 }
