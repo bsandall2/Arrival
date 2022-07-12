990cf56e998d5bbb4ecf9154029fe799ff726dc3
Bryce Sandall <1024240@student.sae.edu.au>
2022-07-12 12:31:51 +1000

    Player health drops over time

--- Assets/Scripts/PlayerHealthScript.cs
+++ Assets/Scripts/PlayerHealthScript.cs
@@ -6,16 +6,33 @@ public class PlayerHealthScript : MonoBehaviour
 {
     public float currentHealth;
     public float maxHealth = 100;
+    public float coef = 0.6f;
+    public float addOverTime = 0.2f;
+
+    PlayerController player;
 
     // Start is called before the first frame update
     void Start()
     {
+        player = GameObject.Find("Player").GetComponent<PlayerController>();
         currentHealth = 100;
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
+        if (player.isDead == false)
+        {
+            currentHealth -= coef * Time.deltaTime;
+            coef += addOverTime * Time.deltaTime;
+        }
+        
+        if (currentHealth <= 0)
+        {
+            player.isDead = true;
+            player.velocity.x = 0;
+        }
+
         Debug.Log(currentHealth);
     }
 
