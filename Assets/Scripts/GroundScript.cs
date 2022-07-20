0ff80ba3926f0314dcb976fde67b164e08367ac7
Bryce Sandall <86949903+bsandall2@users.noreply.github.com>
2022-07-20 12:38:05 +1000

    Fixed platform spawns & rapid spawn bug

--- Assets/Scripts/GroundScript.cs
+++ Assets/Scripts/GroundScript.cs
@@ -22,7 +22,7 @@ public class GroundScript : MonoBehaviour
         player = GameObject.Find("Player").GetComponent<PlayerController>();
         collider = GetComponent<BoxCollider2D>();
         groundHeight = transform.position.y + (collider.size.y / 2);
-        screenRight = Camera.main.transform.position.x * 2;
+        screenRight = Camera.main.transform.position.x * 2.5f;
     }
 
     // Start is called before the first frame update
@@ -44,7 +44,7 @@ public class GroundScript : MonoBehaviour
 
         groundRight = transform.position.x + (collider.size.x / 2);
 
-        if (groundRight < -10)
+        if (groundRight < -20)
         {
             Destroy(gameObject);
             return;
@@ -64,64 +64,68 @@ public class GroundScript : MonoBehaviour
 
     void generateGround()
     {
-        GameObject go = Instantiate(gameObject);
-        BoxCollider2D goCollider = go.GetComponent<BoxCollider2D>();
-        Vector2 pos;
-
-        float h1 = player.jumpVelocity * player.maxHoldJumpTime;
-        float t = player.jumpVelocity / -player.gravity;
-        float h2 = player.jumpVelocity * t + (0.5f * (player.gravity * (t * t)));
-        float maxJumpHeight = h1 + h2;
-        float maxY = player.transform.position.y + maxJumpHeight;
-        maxY *= 0.7f;
-        float minY = 5f;
-        float actualY = Random.Range(minY, maxY);
-
-        pos.y = actualY - goCollider.size.y / 2; ;
-        if (pos.y > 2)
+        if (player.velocity.x > 0)
         {
-            pos.y = 2;
+            GameObject go = Instantiate(gameObject);
+            BoxCollider2D goCollider = go.GetComponent<BoxCollider2D>();
+            Vector2 pos;
+
+            float h1 = player.jumpVelocity * player.maxHoldJumpTime;
+            float t = player.jumpVelocity / -player.gravity;
+            float h2 = player.jumpVelocity * t + (0.5f * (player.gravity * (t * t)));
+            float maxJumpHeight = h1 + h2;
+            float maxY = player.transform.position.y + maxJumpHeight;
+            maxY *= 0.7f;
+            float minY = 5f;
+            float actualY = Random.Range(minY, maxY);
+
+            pos.y = actualY - goCollider.size.y / 2; ;
+            if (pos.y > 2)
+            {
+                pos.y = 2;
+            }
+
+            float t1 = t + player.maxHoldJumpTime;
+            float t2 = Mathf.Sqrt((2.0f * (maxY - actualY)) / -player.gravity);
+            float totalTime = t1 + t2;
+            float maxX = totalTime * player.velocity.x;
+            maxX *= 0.7f;
+            maxX += groundRight;
+            float minX = screenRight + 5;
+            float actualX = Random.Range(minX, maxX);
+
+            pos.x = actualX + goCollider.size.x /2;
+            go.transform.position = pos;
+
+            GroundScript goGround = go.GetComponent<GroundScript>();
+            goGround.groundHeight = go.transform.position.y + (goCollider.size.y / 2);
+
+            int obstacleNum = Random.Range(0, 3);
+            for (int i=0; i<obstacleNum; i++)
+             {
+                 GameObject box = Instantiate(boxTemplate.gameObject);
+                float y = goGround.groundHeight;
+                float halfWidth = goCollider.size.x / 2 - 1;
+                float left = go.transform.position.x - halfWidth;
+                float right = go.transform.position.x + halfWidth;
+                float x = Random.Range(left, right);
+                Vector2 boxPos = new Vector2(x, y);
+                box.transform.position = boxPos;           
+            }
+
+            int healthNum = Random.Range(0, 2);
+            for (int i = 0; i <healthNum; i++)
+            {
+                GameObject healthBox = Instantiate(healthCubeTemplate.gameObject);
+                float y = goGround.groundHeight;
+                float halfWidth = goCollider.size.x / 2 - 1;
+                float left = go.transform.position.x - halfWidth;
+                float right = go.transform.position.x + halfWidth;
+                float x = Random.Range(left, right);
+                Vector2 boxPos = new Vector2(x, y);
+                healthBox.transform.position = boxPos;
+            }
         }
-
-        float t1 = t + player.maxHoldJumpTime;
-        float t2 = Mathf.Sqrt((2.0f * (maxY - actualY)) / -player.gravity);
-        float totalTime = t1 + t2;
-        float maxX = totalTime * player.velocity.x;
-        maxX *= 0.7f;
-        maxX += groundRight;
-        float minX = screenRight + 5;
-        float actualX = Random.Range(minX, maxX);
-
-        pos.x = actualX + goCollider.size.x /2;
-        go.transform.position = pos;
-
-        GroundScript goGround = go.GetComponent<GroundScript>();
-        goGround.groundHeight = go.transform.position.y + (goCollider.size.y / 2);
-
-        int obstacleNum = Random.Range(0, 3);
-        for (int i=0; i<obstacleNum; i++)
-        {
-            GameObject box = Instantiate(boxTemplate.gameObject);
-            float y = goGround.groundHeight;
-            float halfWidth = goCollider.size.x / 2 - 1;
-            float left = go.transform.position.x - halfWidth;
-            float right = go.transform.position.x + halfWidth;
-            float x = Random.Range(left, right);
-            Vector2 boxPos = new Vector2(x, y);
-            box.transform.position = boxPos;           
-        }
-
-        int healthNum = Random.Range(0, 2);
-        for (int i = 0; i <healthNum; i++)
-        {
-            GameObject healthBox = Instantiate(healthCubeTemplate.gameObject);
-            float y = goGround.groundHeight;
-            float halfWidth = goCollider.size.x / 2 - 1;
-            float left = go.transform.position.x - halfWidth;
-            float right = go.transform.position.x + halfWidth;
-            float x = Random.Range(left, right);
-            Vector2 boxPos = new Vector2(x, y);
-            healthBox.transform.position = boxPos;
-        }
+        
     }
 }
